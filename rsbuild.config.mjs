9ec01522f09d411fb849f98521ffc2f6981526bb
huongduongrlypr-source <huongduongrlypr@gmail.com>
2025-10-29 12:38:52 +0000

    Update rsbuild.config.mjs

--- rsbuild.config.mjs
+++ rsbuild.config.mjs
@@ -1,30 +1,9 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 import tailwindcss from '@tailwindcss/postcss';
-import fs from 'fs/promises';
-import path from 'path';
 
 export default defineConfig({
-    plugins: [
-        pluginReact(),
-        {
-            name: 'plugin-htaccess-spa',
-            setup(api) {
-                api.onAfterBuild(async () => {
-                    const distPath = path.resolve('dist');
-                    const htaccessPath = path.join(distPath, '.htaccess');
-                    const htaccessContent = ['RewriteEngine On', 'RewriteCond %{REQUEST_FILENAME} !-f', 'RewriteCond %{REQUEST_FILENAME} !-d', 'RewriteRule ^ index.html [L]'].join('\n');
-                    try {
-                        await fs.access(distPath);
-                        await fs.writeFile(htaccessPath, htaccessContent);
-                        api.logger.info('htaccess build xong');
-                    } catch {
-                        api.logger.error('htaccess build fail');
-                    }
-                });
-            }
-        }
-    ],
+    plugins: [pluginReact()],
     tools: {
         postcss: {
             postcssOptions: {
@@ -49,6 +28,9 @@ export default defineConfig({
         tsconfigPath: './jsconfig.json'
     },
     output: {
+        distPath: {
+            root: 'dist'
+        },
         dataUriLimit: {
             image: Number.MAX_SAFE_INTEGER,
             svg: Number.MAX_SAFE_INTEGER,
